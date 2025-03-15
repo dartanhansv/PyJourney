a297bd1b4257c2f819c0bc27cc3faa29cf54001e
Dartanhan S. Venturini <dartanhan@outlook.com>
2025-03-15 17:56:21 -0300

    convert user input

--- text-to-speech/text-to-speech.py
+++ text-to-speech/text-to-speech.py
@@ -1,8 +1,24 @@
 from gtts import gTTS
 import os
 
-text = "Hello, I am a text to speech program. I can convert text to speech in multiple languages."
-language = "en"
-speech = gTTS(text=text, lang=language, slow=False)
-speech.save("speech-from-text.mp3")
-os.system("speech-from-text.mp3")
+
+# Automatically convert the initial text to speech and play it for the user
+text = "Hello! I am a text-to-speech program. Would you like to give me a try? Be creative!"
+language, tld = "en", "ca"
+speech = gTTS(text=text, lang=language, tld=tld, slow=False)
+speech.save("greeting.mp3")
+os.system("greeting.mp3")
+
+# Continue asking the user for input and converting it to speech
+while True:
+    user_text = input(
+        "\nThe AI revolution is here! What message do you want to send to humanity? "
+        "(Type your text or 'exit' to quit): \n"
+    )
+    if user_text.lower() == "exit":
+        print("Goodbye!")
+        break
+
+    speech = gTTS(text=user_text, lang=language, slow=False)
+    speech.save("text-to-speech.mp3")
+    os.system("text-to-speech.mp3")
